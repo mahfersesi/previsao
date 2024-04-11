5e5de41359b97c619f2ae52d8d2802e2a1a18b69
maria fernanda <mariafernandalibano345@gmail.com>
2024-04-11 11:53:35 -0300

    começando

---
+++ previsao/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace previsao;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ previsao/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace previsao;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
